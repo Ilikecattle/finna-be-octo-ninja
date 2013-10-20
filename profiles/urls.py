6b0beb827663f7c64bc68dbc2f872181c59ff239
Adam Berg <ilikecattle@gmail.com>
2013-10-20 12:06:56 -0700

    Implement the Review page in step by step

--- profiles/urls.py
+++ profiles/urls.py
@@ -4,5 +4,6 @@ from django.conf.urls import patterns, url
 from profiles import views
 
 urlpatterns = patterns('',
-    url(r'^save_session/(\d*)/(\d*)/$', views.save_session, name='savesession')
+    url(r'^save_session/(\d*)/(\d*)/$', views.save_session, name='savesession'),
+    url(r'^review/$', views.review, name='profiles_review')
     )
--- profiles/views.py
+++ profiles/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 from confsessions.models import Session
+from confsessions.views import get_session_times_ordered, get_completed_session_times
 
 from django.contrib.auth.models import User
 
@@ -9,3 +11,12 @@ def save_session(request, session_pk, user_pk):
     user.profile.save_session(session)
     user.save()
     return HttpResponse('Success')
+
+def review(request):
+    session_times = get_session_times_ordered()
+    context = { \
+        'session_times' : session_times, \
+        'completed_session_times' : get_completed_session_times(request.user), \
+        'prev_sessiontime' : session_times[-1] \
+    }
+    return render(request, 'profiles/review.html', context)
