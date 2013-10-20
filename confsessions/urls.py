dc72c3793b33a0ad0916ec25e21932fbe6b195cd
Adam Berg <ilikecattle@gmail.com>
2013-10-20 11:06:27 -0700

    Add names to confsession urls

--- confsessions/urls.py
+++ confsessions/urls.py
@@ -8,7 +8,7 @@ from confsessions import views
 
 urlpatterns = patterns('',
         url(r'^$', views.index, name='sessions'),
-        url(r'^sessiontime/(\d*)', views.sessiontime),
-        url(r'^sessiontype/(\d*)', views.sessiontype),
+        url(r'^sessiontime/(\d*)', views.sessiontime, name='sessiontime'),
+        url(r'^sessiontype/(\d*)', views.sessiontype, name='sessiontype'),
         url(r'^register_session/(\d*)/(\d*)/$', views.register_session, name='register_session'),
         )
