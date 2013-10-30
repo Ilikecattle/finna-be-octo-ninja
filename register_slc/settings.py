ecd8c58dc206e94c1b71b4005834de483e30cb86
Adam Berg <ilikecattle@gmail.com>
2013-10-29 23:41:07 -0700

    Change line endings

--- register_slc/settings.py
+++ register_slc/settings.py
@@ -46,7 +46,7 @@ USE_TZ = True
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
 #MEDIA_ROOT = '/usr/local/www/media/'
-MEDIA_ROOT = 'home/adam/webdev/register_slc/media/'
+MEDIA_ROOT = '/home/adam/webdev/register_slc/media/'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
