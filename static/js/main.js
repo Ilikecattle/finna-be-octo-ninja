b2f49fe8f01f335dc4e94752eabef979433ada52
Adam Berg <ilikecattle@gmail.com>
2013-11-16 09:37:12 -0800

    Don't replace tease with description

--- static/js/main.js
+++ static/js/main.js
@@ -2,6 +2,5 @@ $(document).ready(function() {
 	$('.description').toggleClass('hide');
 	$(".tease_btn").on('click', function() {
 		$(this).prev('.description').toggleClass('hide');
-		$(this).prev().prev('.teaser').toggleClass('hide');
 	});
 });
