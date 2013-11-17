8bf95379a6079a641a9365554d53ca71fda04e62
Adam Berg <adam@xyzdigital.com>
2013-11-17 12:28:58 -0800

    Sweet back and next buttons

--- register_slc/static/js/main.js
+++ register_slc/static/js/main.js
@@ -3,4 +3,12 @@ $(document).ready(function() {
 	$(".tease_btn").on('click', function() {
 		$(this).prev('.description').toggleClass('hide');
 	});
+	function checkOffset() {
+		if ($('.bottom-buttons').offset().top + $('.bottom-buttons').height() >= $('footer').offset().top - 10) $('.bottom-buttons').css('position', 'relative');
+		if ($(document).scrollTop() + window.innerHeight < $('footer').offset().top) $('.bottom-buttons').css('position', 'fixed'); // restore when you scroll up
+	}
+	$(document).scroll(function() {
+		checkOffset();
+	});
+	checkOffset();
 });
